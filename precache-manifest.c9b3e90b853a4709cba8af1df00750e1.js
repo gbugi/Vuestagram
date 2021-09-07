67fbf9d0f2e61669d531e477790164349035a288
거누 <75151693+Pigonhair@users.noreply.github.com>
2021-09-07 10:16:26 +0900

    뷰스타그램

---
+++ precache-manifest.c9b3e90b853a4709cba8af1df00750e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3ae23e87a5cdea5289",
+    "url": "/css/app.cfb4a1b0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ef897609fb8f6a234b46624c7b3c4f67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c3ae23e87a5cdea5289",
+    "url": "/js/app.66419bf0.js"
+  },
+  {
+    "revision": "b1a372915a3a62bd257b",
+    "url": "/js/chunk-vendors.dc45433e.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
